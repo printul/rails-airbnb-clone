63da935140c5ea57401e432a7bdf260a5e3f5e7a
Stephane Mansour <stephane.mansour@gmail.com>
2017-05-29 18:00:52 +0100

    associations on models done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
+  attr_accessor :my_bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :services, :bookings
+  has_many :services
+  has_many :bookings
 
   def my_bookings
-    @my_bookings = User.services.map do |service|
+    @my_bookings = User.find(id)
+    return @my_bookings.services.map do |service|
       service.bookings
     end
-    return @my_bookings
   end
 end
