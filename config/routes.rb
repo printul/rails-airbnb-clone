1b92539b6c32d677ec61381ff03735fe09b73e4d
Hongshu Liu <liuhongshu@gmail.com>
2017-05-30 13:14:29 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,19 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+
+  resources :services, only: [:index, :show]
+
   resources :users, only: [] do
-    resources :bookings, only: [:new, :create, :index, :show, :edit, :update]
+      resources :bookings, only: [:new, :create, :edit, :update, :index, :show]
+  end
+
+  resources :users, only: [] do
+    namespace :vendor do
+      resources :services, only: [:new, :create, :index, :show, :edit, :update] do
+        resources :bookings, only: [:index, :show, :edit, :update]
+      end
+    end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
