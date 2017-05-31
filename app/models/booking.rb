1b9720fe41a10fd6b10f4d10ca122f71c3db4ee1
Stephane Mansour <stephane.mansour@gmail.com>
2017-05-31 12:34:31 +0100

    order status categories added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :service
-  # validates :start_date, :end_date, :order_status, :total_cost, presence: true
+  validates :order_status, inclusion: { in: ["Pending", "Awaiting Payment", "Completed", "Declined", "Accepted"] }
 end
