2fd33e8732597593224053b128111da3359a85bc
Catalin Rusu <printul@gmail.com>
2017-05-31 12:48:26 +0100

    added total cost calculation for bookings/show page

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :service
   # validates :start_date, :end_date, :order_status, :total_cost, presence: true
+  # validates :order_status, presence: true
 end
