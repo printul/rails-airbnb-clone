83112d7fb5e10dea8da178fcb19f9f9d1fec6046
Jason Cheung <jckaichu@gmail.com>
2017-06-01 00:18:11 +0100

    added filter status functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,21 @@
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+
+$('#bookingsSelector').on('change', function() {
+  $('tr').show();
+  if ($('#bookingsSelector')[0].value !== "None") {
+    var arrToHide = [];
+    $('tr td:last-child a').each(function(index) {
+      var status = $(this)[0].text;
+      var filter = $('#bookingsSelector')[0].value;
+      if(status !== filter) {
+        arrToHide.push(index + 1);
+      }
+
+    });
+   arrToHide.reverse().forEach(function(value) {
+    $('tr').eq(value).hide();
+   });
+  }
+});
