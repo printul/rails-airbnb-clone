4765d2e24f01381d9a479ef1355e77c410368d00
Jason Cheung <jckaichu@gmail.com>
2017-05-31 11:50:38 +0100

    added services to user id 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#3 services belonging to user id 1
+service_seed_data = [
+                      {
+                        name: "Very Good Editing",
+                        description: "It's some good editing alright",
+                        daily_rate: 30.0,
+                        user_id: 1,
+                        category: "Editing"
+                      },
+
+                      {
+                        name: "Hollywood-style Filming",
+                        description: "Look like hollywood star",
+                        daily_rate: 333.0,
+                        user_id: 1,
+                        category: "Filming"
+                      },
+
+                      {
+                        name: "Hollywood-style filimng with really good editing",
+                        description: "Look like hollywood star as well as make it look like movie",
+                        daily_rate: 9001.0,
+                        user_id: 1,
+                        category: "Both"
+                      }
+                    ]
+service_seed_data.each { |service_info| Service.create(service_info) }
+
