9873d69791afb9079afe1c8ae9eed66db9650e71
Hongshu Liu <liuhongshu@gmail.com>
2017-06-01 19:41:49 +0100

    booking cancel

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
-  ORDER_STATUSES = ["Pending", "Accepted", "Awaiting Payment", "In Progress", "Completed", "Declined"]
+  ORDER_STATUSES = ["Pending", "Accepted", "Awaiting Payment", "In Progress", "Completed", "Declined", "Cancelled"]
   belongs_to :user
   belongs_to :service
   # validates :start_date, :end_date, :order_status, :total_cost, presence: true
