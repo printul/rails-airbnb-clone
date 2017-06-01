ce4e9da503f7baefaca5cfec7e7998ca1c3042f7
Catalin Rusu <printul@gmail.com>
2017-06-01 10:43:55 +0100

    changed to datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require_tree .
+//= require bootstrap-datepicker
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+//= require_tree .
+
+$(document).on("ready", function(){
+  $(".datepicker").datepicker();
+});
