e17c96f6d797880f8ad9ef01d570887a8c066696
Hongshu Liu <liuhongshu@gmail.com>
2017-06-01 16:12:52 +0100

    updated login modal

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 20170601133320) do
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
     t.float    "total_cost"
-    t.text     "note"
     t.index ["service_id"], name: "index_bookings_on_service_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
